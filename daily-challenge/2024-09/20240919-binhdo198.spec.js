d41a16e0039b8905942e38ef3b9ac02b35c9ef40
Binh Do <binhdt198@gmail.com>
2024-10-12 20:35:39 +0700

    feat: update challenge 2024-09-19

--- daily-challenge/2024-09/20240919-binhdo198.spec.js
+++ daily-challenge/2024-09/20240919-binhdo198.spec.js
@@ -26,9 +26,33 @@
 // - Sau đó, xóa `"Chuối"` khỏi danh sách.
 // - Cuối cùng, tính tổng giá của các sản phẩm trong danh sách còn lại.
 
+const productList = {
+    products: [],
+    addProduct: function addProduct(name, price) {
+        this.products.push({name,price});
+    },
+    removeProduct: function removeProduct(name) {
+        this.products = this.products.filter(product => product.name !== name);
+    },
+    calculateTotal: function calculateTotal() {
+        return this.products.reduce((total,product) => (total + product.price),0);
+    },
+    calculateTotal1: function calculateTotal1() {
+        let total = 0;
+        for(let i = 0; i < this.products.length; i++) {
+            total = total + this.products[i].price
+        }
+        return total
+    }
 
-// addProduct("Táo", 5000);
-// addProduct("Chuối", 3000);
+}
+productList.addProduct("Táo",5000);
+productList.addProduct("Chuối",3000);
+productList.removeProduct("Chuối");
+productList.addProduct("Nho",10000);
+console.log("productList: "+productList.products);
+console.log("total: "+productList.calculateTotal());
+console.log("total1: "+productList.calculateTotal1());
 
 // # Playwright
 // ## Đề bài
@@ -37,3 +61,32 @@
 // - Click vào: Bài học 5: Puzzle drag and drop game
 // - Kéo thả các ô 1, 2, 3, 4 vào ô tương ứng.
 // - Verify message trong alert xuất hiện là: "Congratulations! You completed the puzzle."
+
+const {test, expect} = require('@playwright/test');
+
+test('Challenge 19-09-2024', async ({page}) => {
+    await test.step('Go to page material', async () => {
+        await page.goto('https://material.playwrightvn.com/');
+    })
+    await test.step('Click session 5: Puzzle drag and drop game', async () => {
+        await page.locator("//a[contains(text(),'Bài học 5: Puzzle drag and drop game')]").click();
+    })
+    await test.step('Drag and drop puzzle', async () => {
+        const puzzles = "//div[@class='puzzle-piece']";
+        const numPuzzle = await page.locator(puzzles).count();
+        console.log("Số mảnh ghép: "+numPuzzle);
+
+        page.on('dialog', async dialog => {
+            expect(dialog.message()).toBe("Congratulations! You completed the puzzle.");
+            console.log("message: ",dialog.message());
+            await dialog.accept();
+        })
+
+        for (let i = 1; i <= numPuzzle; i++) {
+            const puzzlePiece = page.locator(`//div[@id='piece-${i}']`);
+            const dropzone = page.locator(`//div[@data-piece='${i}']`);
+            await puzzlePiece.dragTo(dropzone);
+            
+        }
+    })
+})
